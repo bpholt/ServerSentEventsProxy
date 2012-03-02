d357a58acfdc71be209a04b8358ce00911f81100
Brian P. Holt <bholt@planetholt.com>
2012-03-01 23:34:06 -0600

    rest almost done but with a weird teardown that I don't fully understand

--- test/test-RestController.js
+++ test/test-RestController.js
@@ -1,42 +1,73 @@
 (function() {
 	var buster = require('buster'),
 		sinon = require('sinon'),
-		classToTest,
-		body = 'wow!',
-		req = {body: body,
-			   params: {id: 0}},
-		res = {writeHeader: sinon.spy(),
-			   write: sinon.spy(),
-			   end: sinon.spy()};
+		classToTestProto = require('../lib/RestController');
 
 	buster.testCase("RestController", {
 		setUp: function () {
-			classToTest = require('../lib/RestController'); //new RestController();	
+			this.body = 'wow!';
+			this.classToTest = Object.create(classToTestProto);
+
+			// this.classToTest.messages = [];
+			this.req = {body: this.body,
+			   	 	    params: {id: 0}};
+
+			this.res = {writeHeader: sinon.spy(),
+				   		write: sinon.spy(),
+				   		end: sinon.spy()};
 		},
 
-		"handleMessage is a function": function () {
-			assert.equals('function', typeof (classToTest.handleMessage));	
+		tearDown: function () {
+			classToTestProto.messages = [];
 		},
 
 		"message body is stored": function () {
-			classToTest.handleMessage(req, res);
-			assert.equals(body, classToTest.messages[0]);
-			assert.equals(true, res.end.called);
+			this.classToTest.handleMessage(this.req, this.res);
 
-			assert.equals(201, res.writeHeader.args[0][0]);
-			assert.equals({'location': '/messages/0'}, res.writeHeader.args[0][1]);
+			assert.equals('wow!', this.classToTest.messages[0]);
+			assert.equals(true, this.res.end.called);
+
+			assert.equals(201, this.res.writeHeader.args[0][0]);
+			assert.equals({'location': '/messages/0'}, this.res.writeHeader.args[0][1]);
 		},
 
-		"getMessage is a function": function () {
-			assert.equals('function', typeof (classToTest.getMessage));
+		"getMessage returns a message and sets the right headers": function () {
+			this.classToTest.messages[0] = this.body;
+
+			this.classToTest.getMessage(this.req, this.res);
+
+			assert.equals(this.body, this.res.write.args[0][0]);
+			assert.equals(200, this.res.writeHeader.args[0][0]);
+			assert.equals({'content-type': 'application/json'}, this.res.writeHeader.args[0][1]);
 		},
 
-		"getMessage returns the message at the passed id": function () {
-			classToTest.messages[0] = body;
+		"getMessage returns the correct message": function () {
+			this.req.params.id = 1;
+			this.classToTest.messages[1] = 'wowza';
 
-			classToTest.getMessage(req, res);
+			this.classToTest.getMessage(this.req, this.res);
 
-			assert.equals(body, res.write.args[0][0]);
+			assert.equals('wowza', this.res.write.args[0][0]);
+		},
+
+		"getMessage returns 404 when the passed ID doesn't exist": function () {
+			this.req.params.id = 42;
+			this.classToTest.messages = [];
+
+			this.classToTest.getMessage(this.req, this.res);
+
+			assert.equals(404, this.res.writeHeader.args[0][0]);
+			assert.equals(0, this.res.write.callCount);
+		},
+
+		"multiple messages can be pushed": function () {
+			this.classToTest.handleMessage(this.req, this.res);
+			this.req.body = 'shazam';
+
+			this.classToTest.handleMessage(this.req, this.res);
+
+			assert.equals({'location': '/messages/1'}, this.res.writeHeader.args[1][1]);
+			assert.equals('shazam', this.classToTest.messages[1]);
 		}
 	});
 }());
